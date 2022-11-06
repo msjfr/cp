8bcb84ca1018c36ae16edcf5cd2ff0f5dd052ea5
msjfr <117559753+msjfr@users.noreply.github.com>
2022-11-06 09:57:03 +0330

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+  main() { 
+  
+  int a,b,s,p,m,n;
+  
+  cin >> a >> b;
+   
+
+   
+   s=a*b;
+   p=a%b;
+   m=a+b;
+   n=a-b;
+   
+   cout << s<<" " <<p << "  "<< m <<" "<< n;
+}
