658a940858d272377c4db1f4055307726c423b0d
msjfr <117559753+msjfr@users.noreply.github.com>
2022-11-11 10:59:16 +0330

    Add files via upload

---
+++ 52.cpp - Copy.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+ using namespace std;
+
+  main() {
+    int alphabet ;
+	cout<<"please enter alphabet number : ";
+    cin>> alphabet ;	
+    
+	switch ( alphabet){
+		case 1:
+			cout<<"a";
+			break ;
+		case 2 :
+			cout<<"e";
+			break ;
+		case 3 :
+			cout<<"i";
+		case 4 :
+			cout<<"o";
+			break ;
+		case 5 :
+			cout<<"u";
+	}
+}
