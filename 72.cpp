542f32d54be70657b3779f8f3a42511a3c4beba2
msjfr <117559753+msjfr@users.noreply.github.com>
2022-11-24 12:14:16 +0330

    Add files via upload

---
+++ 72.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std ;
+
+int main() {
+	
+	 int i,n,m, s ,div = 0 ;
+	 cout << "enter  n :   ";
+	cin >> n ;
+	  cout << " enter m :  ";
+	cin >> m ;
+	
+	for ( i = 0 ; i <= n ; i++ ){
+		
+		s = m * n ;
+		div = s ;
+	}    
+	
+	cout << " div is :  " << div ;
+}
